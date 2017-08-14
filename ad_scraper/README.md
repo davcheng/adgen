ffac0f59ba35507c4cdecd2735de761e14dc84a9
awsmdavid <awsmdavid@gmail.com>
2017-08-14 15:28:48 -0400

    pushing content

---
+++ ad_scraper/README.md
@@ -0,0 +1,22 @@
+# Webscraper of text ads from search engines
+
+Creates a db (or an output txt file) with text ads for products
+
+written in python 3 + beautiful soup
+
+
+To Run:
+1. install beautiful soup 
+https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup
+
+2. cmd line
+```bash
+python3 ad_scraper.py
+```
+
+3. profit
+
+NOTE: bing not ready for primetime yet...
+
+bs4 documentation:
+https://www.crummy.com/software/BeautifulSoup/bs4/doc/
---
+++ ad_scraper/ad_scraper.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+import re
+from urllib.request import urlopen
+import urllib
+
+# array of objects
+item_list = ["phone", "smartphone"]
+
+# create db to store
+# conn = sqlite3.connect('ads.db')
+# c = conn.cursor()
+# c.execute('DROP TABLE IF EXISTS ads;')
+# c.execute('CREATE TABLE ads ( id integer primary key autoincrement, item text not null, ad_text text);')
+
+# main ad scraper
+def get_ads_for_item(item):
+
+    # get google ads
+    google_ad_scrape(item)
+    # bing_ad_scrape(item)
+
+
+def google_ad_scrape(item):
+    google_address = "http://www.google.com/search?q={0}&num=100&hl=en&start=0".format(item)
+
+    # spoofing agent
+    request = urllib.request.Request(google_address, None, {'User-Agent':'Mosilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
+    urlfile = urlopen(request)
+    page = urlfile.read()
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # get headline text
+    for node in soup.findAll(attrs={'class': re.compile(r".*\bads-creative\b.*")}):
+        # headline
+        print(node.text)
+        write_to_output_file(node.text)
+        # subtext
+        print(node.findNext('div').contents[0])
+        write_to_output_file(node.findNext('div').contents[0])
+
+
+def bing_ad_scrape(item):
+    bing_address = "https://www.bing.com/search?q={0}".format(item)
+
+    # spoofing agent
+    request = urllib.request.Request(bing_address, None, {'User-Agent':'Mosilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
+    urlfile = urlopen(request)
+    page = urlfile.read()
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # get body text
+    for node in soup.findAll(attrs={'class': re.compile(r".*\bb_secondaryText\b.*")}):
+        # print(node.findPrevious('p').contents[0])
+        # write_to_output_file(node.findPrevious('p').contents[0])
+        for text_line in node.contents:
+            print(text_line)
+            write_to_output_file(text_line)
+
+
+def write_to_output_file(write_content):
+    with open("Output.txt", "a") as output_file:
+        output_file.write(write_content)
+        output_file.write("\n")
+
+if __name__ == '__main__':
+    for item in item_list:
+        get_ads_for_item(item)
